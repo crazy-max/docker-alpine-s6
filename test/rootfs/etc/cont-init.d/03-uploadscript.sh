c09c8594df6d32341c7d2744466e20349c197bd3
CrazyMax <crazy-max@users.noreply.github.com>
2020-09-14 18:00:14 +0200

    Add test workflow

---
+++ test/rootfs/etc/cont-init.d/03-uploadscript.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv sh
+
+#UPLOADSCRIPT=${UPLOADSCRIPT:-/data/uploadscript.sh}
+
+if [ -z "$UPLOADSCRIPT" ]; then
+  exit 0
+fi
+if [ ! -f "$UPLOADSCRIPT" ]; then
+  >&2 echo "ERROR: UPLOADSCRIPT program/script is not defined or does not exist"
+  exit 1
+fi
+
+mkdir -p /etc/services.d/pure-uploadscript
+cat > /etc/services.d/pure-uploadscript/run <<EOL
+#!/usr/bin/execlineb -P
+with-contenv
+pure-uploadscript -p /var/run/pure-uploadscript.pid -r ${UPLOADSCRIPT}
+EOL
+chmod +x /etc/services.d/pure-uploadscript/run
